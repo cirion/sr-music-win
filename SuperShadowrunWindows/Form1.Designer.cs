769624cc197df32ad3613f637b60fce583fd153d
Chris King <cirion@gmail.com>
2017-08-06 14:47:49 -0700

    Initial commit.

---
+++ SuperShadowrunWindows/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SuperShadowrunWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.replaceButton = new System.Windows.Forms.Button();
+            this.restoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Navigate to Shadowrun Hong Kong install location.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Shadowrun Hong Kong (SRHK.exe) | SRHK.exe";
+            this.openFileDialog1.InitialDirectory = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\Shadowrun Hong Kong";
+            this.openFileDialog1.Title = "Browse to Shadowrun Hong Kong";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(120, 30);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // replaceButton
+            // 
+            this.replaceButton.Location = new System.Drawing.Point(13, 57);
+            this.replaceButton.Name = "replaceButton";
+            this.replaceButton.Size = new System.Drawing.Size(75, 23);
+            this.replaceButton.TabIndex = 3;
+            this.replaceButton.Text = "Replace Music Files";
+            this.replaceButton.UseVisualStyleBackColor = true;
+            this.replaceButton.Click += new System.EventHandler(this.replaceButton_Click);
+            // 
+            // restoreButton
+            // 
+            this.restoreButton.Location = new System.Drawing.Point(13, 87);
+            this.restoreButton.Name = "restoreButton";
+            this.restoreButton.Size = new System.Drawing.Size(75, 23);
+            this.restoreButton.TabIndex = 4;
+            this.restoreButton.Text = "Restore Original Music";
+            this.restoreButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 342);
+            this.Controls.Add(this.restoreButton);
+            this.Controls.Add(this.replaceButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Shadowrun Hong Kong Music Switcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button replaceButton;
+        private System.Windows.Forms.Button restoreButton;
+        private System.Windows.Forms.Button browseButton;
+    }
+}
+
